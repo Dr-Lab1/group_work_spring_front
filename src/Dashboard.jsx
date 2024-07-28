40f77fa48bca33215c1f1d34dad109a36439c7db
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-07-28 20:41:59 +0100

    ...

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -17,11 +17,11 @@ function Dashboard() {
 
       <Header />
       <SideBar />
-      {/* <Home />
-      <Profil /> 
-      <Faq /> 
-      <Contact /> 
-      <ShowUsers /> */}
+      {/* <Home /> */}
+      {/* <Profil />  */}
+      {/* <Faq />  */}
+      {/* <Contact />  */}
+      {/* <ShowUsers />  */}
       <OffersList />
       <Footer />
 
